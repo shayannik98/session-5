bd868946f11cd36ae839c019c8147c6ffc6bd74d
shayan nikpour <122686935+shayannik98@users.noreply.github.com>
2023-08-07 22:52:34 +0330

    Edition  homework1.py

--- homework1.py
+++ homework1.py
@@ -1,13 +1,13 @@
-ss="#*"
-ws="*#"
+ss="*"
+ws="#"
 def shatrang(row , col):
     for i in range(row):
-           for j in range(col):
-               if i %2==0:
-                    print(ss , end=" ")
+     for j in range(col):
+               if (i+j) %2==0:
+                    print((ss), end="")
                else:
-                    print(ws ,end=" ")  
-           print()                
+                    print((ws),end="")  
+     print()                
 
 print("welcome ")
 rows = int(input("The number of rows: "))
@@ -16,4 +16,4 @@ print()
 
 shatrang(rows, columns)
 
-        
+    
